3f477fd57e1ccff38c8bb2fbe31a3a433cae4970
david95110 <hhhuynh@.fr>
2022-03-31 12:06:00 +0200

    Creation class DaoPersonne

--- app/src/main/java/com/example/listview1/ListViewActivity.java
+++ app/src/main/java/com/example/listview1/ListViewActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.listview1.controller.DaoPersonne;
 import com.example.listview1.model.Personne;
 
 import java.util.ArrayList;
@@ -17,6 +20,7 @@ public class ListViewActivity extends AppCompatActivity {
     //on definit un ArrayAdaptateur pour adapter la liste des
     // personnes au listview
     ArrayAdapter<Personne> personneArrayAdapter;
+    public static final String PERSONNE = "personne";
 
 
 
@@ -35,5 +39,23 @@ public class ListViewActivity extends AppCompatActivity {
         personneArrayAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,personnes);
         listView.setAdapter(personneArrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view,
+                                    int index,   //position
+                                    long l) {
+                // on recupere la personne sur laquelle on a cliqué
+                Personne personne = DaoPersonne.getAllPersonnes().get(index);
+                // on créé un Intent
+                 Intent intent1 = new Intent();
+                 //on stocke la paesonne recupérée dans l'intent
+                intent1.putExtra(PERSONNE,personne);
+
+                }
+
+            }
+        });
     }
 }
